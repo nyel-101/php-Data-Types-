61f42893d393a3025b23b804f2308db4ad0cdbb4
nyel-101 <dandevjr@gmail.com>
2025-02-12 10:36:20 +0000

    test(ui): update caching layer

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2a6ae8aeff54fd5a493972e27f6c063 = () => {
-  return '2026-04-20T19:28:50Z';
+const node_f35cc5d245a341afb304fba3651b30c9 = () => {
+  return '2025-02-12T10:36:20Z';
 };
