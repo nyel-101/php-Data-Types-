69e5b13187c1f0d6777df3af3aa85fbb55355b0c
nyel-101 <dandevjr@gmail.com>
2025-02-21 15:25:34 +0000

    fix(deps): refactor logging interface

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bac7a6ae86f433cae1ff99e1e3925dd = () => {
-  return '2026-03-08T08:01:21Z';
+const node_565f1e284b124cf8a6aa3d6d61762ae6 = () => {
+  return '2025-02-21T15:25:34Z';
 };
