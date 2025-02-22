cdc84a2692759443af9a6e65089aacb3f9a9fe4b
nyel-101 <dandevjr@gmail.com>
2025-02-22 09:25:25 +0000

    docs(api): fix database index

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab9c3c40897947a9ae8cfa8f58c70521 = () => {
-  return '2025-02-20T12:48:45Z';
+const node_23996d1dfe3c4dedb51d005134e58c28 = () => {
+  return '2025-02-22T09:25:25Z';
 };
