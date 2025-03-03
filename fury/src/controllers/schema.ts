c55c9741258aa519ec90a466800cb46ffe9df6bd
nyel-101 <dandevjr@gmail.com>
2025-03-03 03:51:37 +0000

    test(api): sanitize auth tokens

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6dd23fb4c1d646daab5a4e6a90b97a94 = () => {
-  return '2026-05-29T18:10:37Z';
+const node_f8b9a3f20da5459e87b072a1ecfd46d6 = () => {
+  return '2025-03-03T03:51:37Z';
 };
