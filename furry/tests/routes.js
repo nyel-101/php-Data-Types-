d529b46606392deeef372b4c6c40449850ad6d3d
nyel-101 <dandevjr@gmail.com>
2025-03-08 12:34:28 +0000

    feat(worker): decouple payload parser

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0b717ecfe9e43ee8f18064e0a2fad0b = () => {
-  return '2026-01-09T04:21:04Z';
+const node_ff579e9e76384f55b28199a4419ae72c = () => {
+  return '2025-03-08T12:34:28Z';
 };
