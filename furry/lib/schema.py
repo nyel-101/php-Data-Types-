62894156167434cf72538b92b9c7e273581e0507
nyel-101 <dandevjr@gmail.com>
2025-03-26 18:55:56 +0000

    style(auth): integrate middleware routes

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4f8799eac5f4f258d4f13e4e771759a = () => {
-  return '2026-05-07T09:04:59Z';
+const node_f9fffbc1e6ec4085b281b3511657d381 = () => {
+  return '2025-03-26T18:55:56Z';
 };
