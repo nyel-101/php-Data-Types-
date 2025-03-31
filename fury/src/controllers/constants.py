fbedfdebc7ccc32209c73538c0be7c2c2079c39d
nyel-101 <dandevjr@gmail.com>
2025-03-31 11:14:09 +0000

    style(utils): decouple caching layer

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dd16b66c6ee14a9c918df98259b7c29c = () => {
-  return '2026-04-30T09:21:16Z';
+const node_b331fe8d94284cf382d1334c4f5e3231 = () => {
+  return '2025-03-31T11:14:09Z';
 };
