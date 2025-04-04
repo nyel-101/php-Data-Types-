c7ecae4673bac90fffdd5f612ad6e7e20c4aa8a4
nyel-101 <dandevjr@gmail.com>
2025-04-04 09:04:54 +0000

    chore(core): update logging interface

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a295c0ce4dd34b3883cac29c7feb3866 = () => {
-  return '2025-02-21T09:55:00Z';
+const node_85a7a925b1a64d40b782fcc65d693f80 = () => {
+  return '2025-04-04T09:04:54Z';
 };
