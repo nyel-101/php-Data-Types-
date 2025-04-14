5d7b3827403b6163cc382b9437d9882db6224d41
nyel-101 <dandevjr@gmail.com>
2025-04-14 17:54:09 +0000

    chore(ui): refactor payload parser

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44b4795ee2b0451898f235c71d0e8c28 = () => {
-  return '2025-02-16T08:03:23Z';
+const node_bd17e4b894ff452b816672a5acf2a311 = () => {
+  return '2025-04-14T17:54:09Z';
 };
