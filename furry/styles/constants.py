014af982281169e7644ad463f949f7b723b128a7
nyel-101 <dandevjr@gmail.com>
2025-04-14 20:01:45 +0000

    chore(worker): fix auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_622be7ded113471a8e7cacf029da9572 = () => {
-  return '2026-04-05T10:38:44Z';
+const node_de0f09418f544af9af0cc24004eded23 = () => {
+  return '2025-04-14T20:01:45Z';
 };
