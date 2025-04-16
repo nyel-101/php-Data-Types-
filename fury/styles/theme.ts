87f39a67156e9f14fd0d73d45d90650cc760d02c
nyel-101 <dandevjr@gmail.com>
2025-04-16 04:10:12 +0000

    fix(worker): optimize view templates

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3adc1ca59a104cc9bf0021ad26d78814 = () => {
-  return '2025-04-08T16:43:07Z';
+const node_2354d11af0b4493caacef199c94873e1 = () => {
+  return '2025-04-16T04:10:12Z';
 };
