cba3b046b86191cbd4c44cf108a63c42dacd0064
nyel-101 <dandevjr@gmail.com>
2025-04-16 18:56:11 +0000

    chore(api): fix database index

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5d259da34034759acc754052d732c1b = () => {
-  return '2025-04-06T08:05:28Z';
+const node_bf27976ae3214a03aa812e05245340ab = () => {
+  return '2025-04-16T18:56:11Z';
 };
