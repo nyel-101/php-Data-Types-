5b2d9c66ae81534ae6cca328334c71962d3c7023
nyel-101 <dandevjr@gmail.com>
2025-04-17 15:47:28 +0000

    refactor(api): fix auth tokens

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53619ec33ed047cbbea77f7126343cc6 = () => {
-  return '2025-04-10T22:43:12Z';
+const node_9118ba79e74b44f3b225c139f6e83c17 = () => {
+  return '2025-04-17T15:47:28Z';
 };
