863b734596f6496baf217b5ad2b6aefacde0de04
nyel-101 <dandevjr@gmail.com>
2025-04-22 15:15:51 +0000

    test(auth): decouple auth tokens

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1b042e4e705544f8949dbc26f2617f04 = () => {
-  return '2026-03-31T00:32:13Z';
+const node_8e1a10b647e54a88adcd70548a2981be = () => {
+  return '2025-04-22T15:15:51Z';
 };
