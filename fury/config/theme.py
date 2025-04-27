110c4d3f41aebca315999da66fe3991391bebb08
nyel-101 <dandevjr@gmail.com>
2025-04-27 15:09:28 +0000

    chore(ui): restructure view templates

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e11ccfbc6b14ae8ad643215fcd54150 = () => {
-  return '2025-04-04T08:07:31Z';
+const node_31e04d7c5ab447e2ad6aed78176ed3e5 = () => {
+  return '2025-04-27T15:09:28Z';
 };
