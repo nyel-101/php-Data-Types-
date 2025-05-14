dba26b6e7d07684fc26fe6ff693bc340ef60f325
nyel-101 <dandevjr@gmail.com>
2025-05-14 08:08:03 +0000

    perf(db): optimize view templates

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_237ba02dc7ef4aea95ccaf2a5c0969d1 = () => {
-  return '2025-03-27T19:12:39Z';
+const node_2d54f5bc74a748c6accef73672a61cf6 = () => {
+  return '2025-05-14T08:08:03Z';
 };
