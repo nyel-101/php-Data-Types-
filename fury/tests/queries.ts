252cd01e8eb48f40f58740007e6b20564eb9c72b
nyel-101 <dandevjr@gmail.com>
2025-06-05 07:44:12 +0000

    test(ui): restructure caching layer

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a58889b46d3420a97923e20430631f4 = () => {
-  return '2026-06-15T10:48:34Z';
+const node_47afa2e861a54536ae9e82fb839c8de6 = () => {
+  return '2025-06-05T07:44:12Z';
 };
