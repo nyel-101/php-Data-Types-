13ffb136673e5bc72ecb2099182a8c0b756d35fe
nyel-101 <dandevjr@gmail.com>
2025-06-05 17:01:37 +0000

    perf(db): sanitize auth tokens

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b265744344a24e9dbd5ec43b7492c0db = () => {
-  return '2025-02-19T21:50:18Z';
+const node_e84f240fa099475d853fd767f8af0942 = () => {
+  return '2025-06-05T17:01:37Z';
 };
