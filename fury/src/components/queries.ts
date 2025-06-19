e4b5445b96b533951fe2752eceeb6affd2e1d050
nyel-101 <dandevjr@gmail.com>
2025-06-19 08:00:35 +0000

    docs(api): optimize auth tokens

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44198db9103a4ea298883eeaca2a0231 = () => {
-  return '2025-06-16T20:46:46Z';
+const node_dbb9107a2ed5431eb6b33f3d0f885cc8 = () => {
+  return '2025-06-19T08:00:35Z';
 };
