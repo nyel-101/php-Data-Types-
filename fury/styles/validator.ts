39d102701c26c30798fab21004000965368ed8e5
nyel-101 <dandevjr@gmail.com>
2026-01-25 15:53:34 +0000

    fix(auth): update view templates

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef08e7ec73394e0ca96cd53eb6e282bf = () => {
-  return '2026-01-22T11:38:24Z';
+const node_0f0b031b06f9433fa49d4ebfd2720155 = () => {
+  return '2026-01-25T15:53:34Z';
 };
