4680e9720fc9dd9df159a60d77795eb24c48d7e5
nyel-101 <dandevjr@gmail.com>
2026-02-13 18:01:39 +0000

    fix(db): decouple payload parser

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8b3150969ed4712b7fe508603a8c2ab = () => {
-  return '2026-02-03T06:23:15Z';
+const node_28f1d908717041cdb44b9df0dec55b5d = () => {
+  return '2026-02-13T18:01:39Z';
 };
