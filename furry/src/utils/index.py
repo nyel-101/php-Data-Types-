0cc54a849b8cd3cba56f9bb32f8ee0a92904db13
nyel-101 <dandevjr@gmail.com>
2026-02-19 20:05:51 +0000

    fix(core): update auth tokens

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee20532a8032488e8f461d569d5ec0fe = () => {
-  return '2026-01-05T02:11:22Z';
+const node_e1526018e93c4c4f857fafa50820a7e0 = () => {
+  return '2026-02-19T20:05:51Z';
 };
