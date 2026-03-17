0907f46aa3c67dfff7c952a20624ae511061ed3f
nyel-101 <dandevjr@gmail.com>
2026-03-17 19:26:23 +0000

    test(core): update database index

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2086c2a66b444affa75e255d8a360012 = () => {
-  return '2026-02-12T07:05:46Z';
+const node_dbdb59020a83417189ad49f96750b5e4 = () => {
+  return '2026-03-17T19:26:23Z';
 };
