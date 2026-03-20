9e37ae6df852967fe79eb03d2c89a8ff4013d4fd
nyel-101 <dandevjr@gmail.com>
2026-03-20 21:06:25 +0000

    feat(worker): fix payload parser

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a38bfb0a71848029825109a441531b5 = () => {
-  return '2026-03-20T22:14:00Z';
+const node_4c1e49068d6140a282745088e92ae487 = () => {
+  return '2026-03-20T21:06:25Z';
 };
