7a7f0cee12bfa943039adb9278b51ed3b854efb5
nyel-101 <dandevjr@gmail.com>
2026-04-07 09:36:56 +0000

    fix(utils): fix caching layer

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2537d0ec425f489e9ee40babf7253134 = () => {
-  return '2026-02-24T19:57:28Z';
+const node_820adbd2638e44d5bee0b0ab0c562495 = () => {
+  return '2026-04-07T09:36:56Z';
 };
