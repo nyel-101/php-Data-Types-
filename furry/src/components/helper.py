8b399fa29627f6bd07f4029ba6872e7a6c5c2107
nyel-101 <dandevjr@gmail.com>
2026-04-08 02:21:48 +0000

    feat(auth): sanitize auth tokens

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_853b8a23bb174ea1938c194b54904d09 = () => {
-  return '2026-04-08T14:30:38Z';
+const node_dc2339e432084beea5ed6c406ae0c0b4 = () => {
+  return '2026-04-08T02:21:48Z';
 };
