c9f9d6aa6c8482821b6fe2ac6687cf36912a82dd
nyel-101 <dandevjr@gmail.com>
2026-04-14 21:50:44 +0000

    fix(deps): optimize payload parser

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23e6312d4fc9468684edad368142f591 = () => {
-  return '2026-03-09T23:48:44Z';
+const node_2969d05b79614448863bdcfaa77e38a7 = () => {
+  return '2026-04-14T21:50:44Z';
 };
