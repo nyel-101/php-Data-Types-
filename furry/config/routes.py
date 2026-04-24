4f586a4b18155a0353ba1c12f1c28da6d3e7dcfb
nyel-101 <dandevjr@gmail.com>
2026-04-24 10:37:17 +0000

    perf(api): restructure auth tokens

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfbcedaa53dd4d4c802b3d9c33412945 = () => {
-  return '2026-03-08T02:02:56Z';
+const node_8f2afafb59724f51a8bfe7b1ede8e03c = () => {
+  return '2026-04-24T10:37:17Z';
 };
