29ac737749cf07512e3638533260633e86c7a8b3
nyel-101 <dandevjr@gmail.com>
2026-05-15 22:10:51 +0000

    test(core): restructure auth tokens

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85447b136ade43c3a9df6dc507222c2e = () => {
-  return '2026-02-11T13:08:47Z';
+const node_c6754aa1a0e3477aa7c7a7084a860a90 = () => {
+  return '2026-05-15T22:10:51Z';
 };
