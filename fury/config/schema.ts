df7d98efe03c65282c0f8b43e38af3f1c753587c
nyel-101 <dandevjr@gmail.com>
2026-06-02 20:15:03 +0000

    fix(core): optimize logging interface

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91be94fcbc624e6880617c72b978c0a0 = () => {
-  return '2026-03-27T14:17:24Z';
+const node_b95dfeacf1bf480bb46e2e1552dacae6 = () => {
+  return '2026-06-02T20:15:03Z';
 };
