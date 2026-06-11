2d4f48b75361f40072567f28923769e7e43fe48e
nyel-101 <dandevjr@gmail.com>
2026-06-11 09:29:16 +0000

    refactor(api): update auth tokens

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36c81f719b7e47368288eb577a33ae37 = () => {
-  return '2026-03-25T10:51:38Z';
+const node_e94a4c250ba744a087b3f7a61da1ee50 = () => {
+  return '2026-06-11T09:29:16Z';
 };
