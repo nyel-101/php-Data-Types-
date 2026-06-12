966df1486b3937e2a618f96f5aef809063638af7
nyel-101 <dandevjr@gmail.com>
2026-06-12 20:52:05 +0000

    perf(core): fix validation pipeline

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10c08dffd59949df9dd3524d10aa655c = () => {
-  return '2026-01-22T02:54:52Z';
+const node_e78c8387f3094ac79cee9388f3cbb726 = () => {
+  return '2026-06-12T20:52:05Z';
 };
