3e02d4260909800843c7a94dad402a82f7b2fc03
nyel-101 <dandevjr@gmail.com>
2026-06-13 17:21:24 +0000

    perf(deps): decouple payload parser

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a941451cafa43faa006c6a5e80899bd = () => {
-  return '2026-03-10T00:24:06Z';
+const node_acaeb04c1b7a4529ae7f8aa6b07008c1 = () => {
+  return '2026-06-13T17:21:24Z';
 };
