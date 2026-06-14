870508b943095131f94c67db00590906c354a5c6
nyel-101 <dandevjr@gmail.com>
2026-06-14 00:26:19 +0000

    perf(core): restructure validation pipeline

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a75c7aa308546929d96c4bbc78c58f9 = () => {
-  return '2026-01-02T22:37:57Z';
+const node_2d95afbcdbd1462e9537e204ae489d4e = () => {
+  return '2026-06-14T00:26:19Z';
 };
